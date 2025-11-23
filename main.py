a5911846ce4ffacf01864c7560736ac518131f63
Finn Beckitt-Marshall <finn@pictura.co.uk>
2025-11-23 12:03:59 +0000

    Update fetch_collection call and enhance get_releases_paginated with search query

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tqdm import tqdm
-
+import tabulate
 from backend import DiscogsManager
 
 class DownloadProgressBar(tqdm):
@@ -9,8 +9,8 @@ class DownloadProgressBar(tqdm):
 
 manager = DiscogsManager()
 
-coll = manager.fetch_collection(force_update=True, progress_callback=DownloadProgressBar().update_to)
+coll = manager.fetch_collection(force_update=False, progress_callback=DownloadProgressBar().update_to)
 manager.fetch_artist_sort_names()
 print(coll)
-releases = manager.get_releases_paginated(page=0, page_size=600, sort_by='artist', desc=False)
-print(releases)
+releases = manager.get_releases_paginated(page=0, page_size=600, sort_by='artist', desc=False, search_query="since")
+print(tabulate.tabulate(releases[0], headers="keys"))  # Print first page of sorted releases
